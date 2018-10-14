9482cd72dd8d58bf827d1e29c218bc5691c71ac8
Denis Rykovanov <denis.rykovanov@mail.ru>
2018-10-14 10:56:34 +0300

    [+] initial implementation

---
+++ README.md
@@ -0,0 +1,31 @@
+Temperature Alarmer
+======
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+It is a basic temperature Alarmer based on the [DS18B20](https://www.aliexpress.com/premium/DS18B20.html?d=y&blanktest=0&origin=y&SearchText=DS18B20&tc=ppc&initiative_id=SB_20181013225623&isViewCP=y&catId=0) sensor and [johnny-five](http://johnny-five.io).
+# Pins 
+* 6 - Alarm Pin
+* 2-5 - Indication of progress reaching the max from the base temperature. Each pin(1-3) gets brightness/pwd signal in one of three ranges
+    * 1 led - 0-33%
+    * 2 led - 33%-66%
+    * 3 led- 66%-100%.
+* 2 - Input data from the sensor
+
+*Important*: You need to have _4.7 kOm_ resister. 
+Refer to the [schema](http://johnny-five.io/examples/temperature-ds18b20/#breadboard-for-thermometer-ds18b20)
+
+
+# How to use
+
+1. Install/Deploy [ConfigurableFirmata](https://github.com/firmata/ConfigurableFirmata) on your arduino device
+1. Install latest [nodejs](https://nodejs.org/)
+1. Run `npm i`
+1. Run `npm start -- --baseTemp <number> --maxIncrease <number>`
+
+    for example:
+        
+        npm run start -- --baseTemp 24 --maxIncrease 0.3
+    
+    also optionally, You could define `serialPort`, by default it detects automatically
+    
+        npm run start -- --baseTemp 24 --maxIncrease 0.3 --serialPort COM5
---
+++ index.js
@@ -0,0 +1,52 @@
+const five = require('johnny-five')
+const argv = require('optimist')
+  .usage('Usage: $0 --baseTemp [num] --maxIncrease [num] --serialPort <PORT>')
+  .demand(['baseTemp', 'maxIncrease'])
+  .default({baseTemp: 22, maxIncrease: 7})
+  .argv
+const baseTemperature = argv.baseTemp
+
+const serialPort = argv.serialPort
+const maxTemperatureIncreasing = argv.maxIncrease
+const maxTemperature = baseTemperature + maxTemperatureIncreasing
+console.log(`Base temperature: base ${baseTemperature.toFixed(2)} °C, max ${maxTemperature.toFixed(2)} °C`)
+
+const board = new five.Board({port: serialPort, repl: false})
+
+const ledPins = [3, 4, 5]
+const alarmLedPin = 6
+const ledCount = ledPins.length
+const ledMaxBrightness = 255
+
+board.on('ready', function () {
+  // This requires OneWire support using the ConfigurableFirmata
+  const thermometer = new five.Thermometer({
+    controller: 'DS18B20',
+    pin: 2
+  })
+
+  console.log('Led pins', ledPins)
+  console.log('Alarm led pin', alarmLedPin)
+  const leds = new five.Leds([...ledPins]) // clone because five mutate/erase array
+  const alarmLed = new five.Led(alarmLedPin)
+
+  thermometer.on('change', function () {
+    const tempPercent = (this.celsius - baseTemperature) / maxTemperatureIncreasing
+    console.log(`${this.celsius.toFixed(2)} / ${maxTemperature.toFixed(2)} °C  | ${(tempPercent * 100).toFixed(2)} %`)
+
+    for (let indx = 0; indx < ledCount; indx++) {
+      const led = leds[indx]
+      const ledPercentTo = (indx + 1) / ledCount
+      const ledPercentFrom = (indx) / ledCount
+      const brightness = Math.min(ledMaxBrightness * (tempPercent - ledPercentFrom) / ledPercentTo, ledMaxBrightness);
+
+      (tempPercent > ledPercentFrom) ? (led.on() && led.brightness(brightness)) : led.off()
+    }
+
+    if (tempPercent >= 1) {
+      alarmLed.on()
+    } else {
+      alarmLed.off()
+    }
+  })
+})
